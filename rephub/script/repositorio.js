cb615f3502a74ebc7da82153ff0b7aa2c7a114b4
Estagio02 <estagioalpha02@outlook.com>
2024-09-07 16:18:42 -0400

    Alterações 07/09 .1

--- rephub/script/repositorio.js
+++ rephub/script/repositorio.js
@@ -1,5 +1,20 @@
-function initializeRep() {
+function getData() {
+    fetch("./content/cards-content.json").then(async (res) => {
+        result = await res.json();
 
+        insertCards(result);
+    })
 }
 
-initializeRep()
+getData()
+
+
+function insertCards(data) {
+    const divCards = document.getElementById("container-cards-master");
+
+    for(info of data) {
+        divCards.innerHTML += `
+        <repcard-component type="${info.type}" name="${info.name}" project-name="${info.projectName}" counter-card="1000" project-category="${info.projectCategory}" image-name="${info.imageSource}"></repcard-component>
+        `
+    }
+}
